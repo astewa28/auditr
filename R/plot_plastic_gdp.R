f95762d52acd66509915d8afe893fffc9c63f655
Justin Mai <jmai25@calpoly.edu>
2026-05-30 17:05:07 -0700

    added Jons plot code

--- R/plot_plastic_gdp.R
+++ R/plot_plastic_gdp.R
@@ -1,7 +1,86 @@
-#' Plot of total plastic vs gdp of each country
+#' density_plotter
 #'
-#' This function creates a plot displaying the total plastic waste by gdp for each country for a specified year.
+#' Plots a density ridge plot comparing the density of plastic type audit amounts to gdp based on user inputs
 #'
-#' @return A ggplot bubble plot with one point for every country.
+#' @param data a data frame with plastic audit numbers and gdp figures
+#' @param selected_year optional, default 2019. 2019 or 2020 for our data
+#' @param cutoff optional, default 3500. the cutoff for the density plot x axis. Can be useful to ignore large out liars or to zoom into the most common audit amounts
+#' @param chosen_types optional, default includes: unknown, hdpe, ldpe, other, pet, pp, ps, pvc. what type of plastic is to be compared to eachother
 #'
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 theme
+
+#'
+#' @returns a plot
 #' @export
+#'
+#' @examples density_ploter(final_merged,
+#'                          selected_year = 2020,
+#'                          cutoff = 2000,
+#'                          chosen_types = c("hdpe", "pvc", "other"))
+density_ploter <- function(data, selected_year = 2019,
+                           cutoff = 3500,
+                           chosen_types = c("unknown", "hdpe", "ldpe","other",
+                                            "pet", "pp", "ps", "pvc")) {
+
+  allowed_types <- c("unknown", "hdpe", "ldpe","other",
+                     "pet", "pp", "ps", "pvc")
+  if (!all(chosen_types %in% allowed_types)) {
+    stop("plastic_type is not in dataframe")
+  }
+
+  allowed_years <- c(2019, 2020)
+  if (!selected_year %in% allowed_years) {
+    stop("year must be either 2019 or 2020")
+  }
+
+  data |>
+    filter(year == selected_year) |>
+    dplyr::group_by(country) |>
+    dlpyr::summarize(unknown = sum(empty),
+                     hdpe = sum(hdpe),
+                     ldpe = sum(ldpe),
+                     other = sum(o),
+                     pet = sum(pet),
+                     pp = sum(pp),
+                     ps = sum(ps),
+                     pvc = sum(pvc),
+                     total = sum(total),
+                     gdp_per_capita = mean(gdp_per_capita),
+                     hdi = mean(HDI),
+                     .groups = "drop") |>
+    mutate(income_group = dplyr::case_when(
+      gdp_per_capita < 1500 ~ "Low Income",
+      gdp_per_capita > 12000 ~ "High Income",
+      TRUE ~ "Middle Income")) |>
+    tidyr::pivot_longer(
+      cols = unknown:pvc,
+      names_to = "plastic_type",
+      values_to = "waste") |>
+    filter(waste <= cutoff, plastic_type %in% chosen_types) |>
+
+    ggplot2::ggplot(mapping = aes(x = waste,
+                                  y = plastic_type,
+                                  fill = income_group)) +
+    ggridges::geom_density_ridges(alpha = 0.7) +
+    ggplot2::scale_fill_manual(values = c(
+      "High Income"   = "#E69F00",
+      "Middle Income" = "#009E73",
+      "Low Income"    = "#0072B2")) +
+    labs(
+      title = "Plastic Waste Type Distribution of Countries by GDP per Capita",
+      subtitle = "Divided into <span style='color: #E69F00;'>High Income</span>,
+      <span style='color: #009E73;'>Middle Income</span>,
+    and <span style='color: #0072B2;'>Low Income</span>",
+      x = "Plastic Waste Units Audited",
+      y = "",
+      alt = "Density ridge plot showing the density of plastic waste audited
+    for multiple types of plastic. Colored by GDP per capita category,
+    High, Middle, and Low") +
+    ggplot2::theme_minimal() +
+    theme(plot.subtitle =  ggtext::element_markdown(),
+          legend.position = "none")
+}

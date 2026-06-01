effb8179eba6878a863654a0661a28de56e532b2
Avery Stewart <astewa28@calpoly.edu>
2026-06-01 14:34:19 -0700

    load_data change

--- R/load_data.R
+++ R/load_data.R
@@ -8,6 +8,8 @@
 
 load_data <- function(){
 
-  readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2021/2021-01-26/plastics.csv')
+  arrow::read_csv_arrow('inst/merged_data.csv')
 
 }
+
+

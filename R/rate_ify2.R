79835d14bb56ce8194822082c147354b0ce929cb
jonflores27 <jonfloresmartinez27@gmail.com>
2026-06-03 17:02:35 -0700

    fixing rateify example

--- R/rate_ify2.R
+++ R/rate_ify2.R
@@ -13,12 +13,12 @@
 #' @export
 #'
 #' @examples
-#' rate_ify2(df = load_data(), numerator = hdpe:pvc, denominator = total_waste)
+#' rate_ify2(df = load_data(), numerator = hdpe:pvc, denominator = grand_total)
 rate_ify2 <- function(df, numerators, denominator) {
   stopifnot(is.data.frame(df))
 
   df |>
-    mutate(across(.cols = {{ numerators }},
+    dplyr::mutate(dplyr::across(.cols = {{ numerators }},
                   .fns = ~ ifelse({{denominator}} == 0,
                                   NA_real_,
                                   .x / {{ denominator }}),
